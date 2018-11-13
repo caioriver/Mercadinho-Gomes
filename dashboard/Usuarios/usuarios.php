454adb76a504961bceef2956c16b4fcd97e6bdc5
caioriver <43602376+caioriver@users.noreply.github.com>
2018-11-13 03:29:23 -0300

    Add files via upload

--- dashboard/Usuarios/usuarios.php
+++ dashboard/Usuarios/usuarios.php
@@ -6,79 +6,114 @@ require_once "../Constructs/header.php";
 <html>
 <link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
 
-<!-- NAVBAR -->
-<div id="mySidenav" class="menu">
-<div id="icom" class = "icom">
-<img src = "../Images/user-icon.png" class="icom-img">
-</div>
-  <div id="list" class="list">
-      <div id="item" class="box">
-        
-        <!-- Menu principal -->
-            <a class="item navitens item_font" href="../Principal/principal.php">Principal</a>
-
-        <!-- Menu usuarios -->          
-            <a class="item navitens item_font click" href="../Usuarios/usuarios.php">Usuários</a>
-
-        <!-- Menu requisições -->
-            <a class="item navitens item_font" href="../Requisicoes/requisicoes_CRUD.php">Requisições</a>
-
-        <!-- Menu estoque -->          
-            <a class="item navitens item_font" href="../Estoque/estoque_CRUD.php">Estoque</a>
-
-        <!-- Menu sair -->
-            <a class="item navitens item_font" href="../Sair/sair.php">Sair</a>
-          
-      </div>
-  </div>
-</div>
-
-<html>
-
 <link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
-<title>Usuários</title>
+
 <link rel="stylesheet" type="text/css" href="../CSS/usuarios.css">
+
+
 <body>
-    <div id="title" class="title">
-<h1>Área do Usuário</h1>
-</div> <!--fim title-->
-    <section id="cards">
-
-        <div class="row justify-content-md-center">
-        
-         <div class="row justify-content-md-center">
-        
-        <div class="col col-lg-2">
-            <a href="../Usuarios/usuarios_criar.php"><div class=" card text-center border-lowblue mb-3" style="max-width: 20rem;">
-                <div class="card-body text-success">
-                    <div class="mt-5 col-12 col-md-4">
-                        <img class="d-flex justify-content-center card-img-top plus-card-img" src="../Images/card-plus.png" alt="Card image cap">  
-                    </div>
-                    <div id="card-foot" class="card-foot" >
-                        Criar usuário
-                    </div>    
-                    </div> <!--fim card col-->
-                </div><!--fim card body-->
-            </div></a> <!--Fim card-->
-        <div> <!--fim coluna-->
-
-        <div class="col col-lg-2">
-            <a href="../Usuarios/usuarios_CRUD.php"><div class=" card text-center border-lowblue " style="max-width: 20rem;">
-                <div class="card-body">
-                    <div class="mt-5 col-12 col-md-4">
-                        <img class="d-flex justify-content-center card-img-top plus-card-img" src="../Images/card-edit.png" alt="Card image cap">  
-                    </div>
-                    <div id="card-foot" class="card-foot">
-                        Editar usuário
-                    </div>    
-                    </div> <!--fim card col-->
-                </div><!--fim card body-->
-            </div></a> <!--Fim card-->
-        </div> <!--fim coluna-->
-        
-        </div> <!--fim Linha-->
-
-        
-    </section>
     
+    <!-- NAVBAR -->
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Mercadinho Gomes</h3>
+                <strong>MG</strong>
+            </div>            
+            <ul class="list-unstyled components menuu">
+                <div class="cordefundo">
+                    <li>
+                        <a href="../Principal/principal.php">
+                            Principal
+                        </a>
+                    </li>
+                    <li class="active">
+                        <a href="../Usuarios/usuarios.php">
+                            Usuarios
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../Requisicoes/requisicoes_CRUD.php">
+                            Requisições
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../Estoque/estoque_CRUD.php">
+                            Estoque
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../Sair/sair.php">
+                            Sair
+                        </a>
+                    </li>  
+                </div>
+            </ul>            
+        </nav>
+
+
+        <!-- Começo da pagina -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                
+
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info botaomenu">
+                        <i class="fas fa-align-left"></i>
+                        <span>Menu</span>
+                    </button>
+                
+                    <div class="title justify-content-center text-center">
+                        <h1>Usuários</h1>
+                    </div>
+
+                </div>
+
+            </nav>
+
+            <section id="cards justify-content-center">
+                <div class="row justify-content-center">
+                    <div class="col-7 col-sm-4 col-md-5 col-lg-4 col-xl-4">
+                        <a href="../admin/graph.php">
+                            <div class="card text-center border-lowblue mb-3  configImagem" >
+                                <div class="card-body text-success">
+                                    <div class="justify-content-center align-content-center align-items-center col-auto imagens">
+                                        <img class="d-flex justify-content-center card-img-top plus-card-img" src="../Images/card-plus.png" alt="Card image cap">
+                                    </div>
+                                    <p class="card-foot">
+                                        Criar Usuário
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-7 col-sm-4 col-md-5 col-lg-4 col-xl-4">
+                        <a href="#">
+                            <div class="card text-center border-lowblue mb-3  configImagem" >
+                                <div class="card-body text-success">
+                                    <div class="justify-content-center align-content-center align-items-center col-auto imagens">
+                                        <img class="d-flex justify-content-center card-img-top plus-card-img" src="../Images/card-edit.png" alt="Card image cap">
+                                    </div>
+                                    <p class="card-foot">
+                                        Editar Usuário
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+                
+                
+            
+            <?php
+                require_once "../Constructs/footer.php"
+            ?>
+            
 </body>
+</html>
