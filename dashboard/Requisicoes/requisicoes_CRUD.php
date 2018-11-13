96618bfd9f88253ac56f579eb5e0f63d4c1f3c15
PauloRMacedo <44329969+PauloRMacedo@users.noreply.github.com>
2018-11-13 16:05:36 -0300

    Fornt-End 16:00

--- dashboard/Requisicoes/requisicoes_CRUD.php
+++ dashboard/Requisicoes/requisicoes_CRUD.php
@@ -7,15 +7,9 @@ require_once "listar_requisicoes.php";
 
 <html>
 <link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
-
-
-
 <link rel="stylesheet" type="text/css" href="../CSS/principal.css">
 
-
-
 <body>
-    
     <!-- NAVBAR -->
     <div class="wrapper">
         <!-- Sidebar  -->
@@ -31,12 +25,12 @@ require_once "listar_requisicoes.php";
                             Principal
                         </a>
                     </li>
-                    <li class="active">
+                    <li>
                         <a href="../Usuarios/usuarios.php">
                             Usuarios
                         </a>
                     </li>
-                    <li>
+                    <li class="active">
                         <a href="../Requisicoes/requisicoes_CRUD.php">
                             Requisições
                         </a>
@@ -55,45 +49,37 @@ require_once "listar_requisicoes.php";
             </ul>            
         </nav>
 
-
         <!-- Começo da pagina -->
         <div id="content">
-
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                
-
                 <div class="container-fluid">
-
                     <button type="button" id="sidebarCollapse" class="btn btn-info botaomenu">
                         <i class="fas fa-align-left"></i>
                         <span>Menu</span>
                     </button>
-                
                     <div class="title justify-content-center text-center">
-                        <h1>Usuários</h1>
+                        <h1>Requisições</h1>
                     </div>
-
                 </div>
-
             </nav>           
         <section>
-         <div class="form-group">
-            <form action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-                <div class="row">
-                    <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
-                        <label class="ml-3 label-select">Categoria:</label>
-                    <select name = "type" class="form-control imput-select">
-                        <option value = "nome">Nome</option>
-                        <option value = "id">ID</option>
-                        <option>Congelados</option>
-                    </select>
-                    </div>
-                    <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
-                        <label class="ml-3 label-serach">Buscar por:</label>
+            <div class="form-group">
+                <form action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                    <div class="row">
+                        <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
+                            <label class="ml-3 label-select">Categoria:</label>
+                        <select name = "type" class="form-control imput-select">
+                            <option value = "nome">Nome</option>
+                            <option value = "id">ID</option>
+                            <option>Congelados</option>
+                        </select>
+                        </div>
+                        <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
+                            <label class="ml-3 label-serach">Buscar por:</label>
                             <input name="busca" type="text" class="form-control imput-serach" placeholder="Nome do item">
-                    </div>
-                    <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
-                        <label class="ml-3 label-order">Ordenar por</label>
+                        </div>
+                        <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
+                            <label class="ml-3 label-order">Ordenar por</label>
                             <select name="order" class="form-control imput-order">
                                 <option value= "1">Mais antigos</option>
                                 <option value = "2">Mais novos</option>
@@ -101,29 +87,29 @@ require_once "listar_requisicoes.php";
                                 <option value = "4">Maior dívida</option>
                                 <option value = "5">Menor dívida</option>
                             </select>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
         </section>        
-            <form method= "GET" action = "requisicoes_validar.php">  
-                <div class="row">
-                    <div class="col-6 col-sm-3 text-center col-md-3 col-lg-3">                        
-                        <button type="submit" class="btn btn-primary ">Buscar</button>
-                    </div>
-                    <div class="col-6 col-sm-3 text-center col-md-3 col-lg-3">                        
-                        <input type=submit  class="btn btn-primary"onclick="window.location.href='requisicoes_validar.php'" value="Deletar multiplos">
-                    </div>
-                    <div class="col-6 col-sm-3 text-center col-md-3 col-lg-3">                        
-                        <button type="submit" class="btn btn-primary">Exportar</button>
-                    </div>
-                    <div class="col-6 col-sm-3 text-center col-md-3 col-lg-3">                        
-                        <input type="button" value="Adicionar" class="btn btn-primary" onclick="window.location.href='requisicoes_validar.php'" >
-                    </div>
+        <form method= "GET" action = "requisicoes_validar.php">  
+            <div class="row">
+                <div class="col-6 col-sm-3 text-center col-md-3 col-lg-3">                        
+                    <button type="submit" class="btn botao btn-primary ">Buscar</button>
                 </div>
-        <section id="table-control justify-content-center">
-            <div class="table-control table-responsive">
-            <table class="table">
+                <div class="col-6 col-sm-3 text-center col-md-3 col-lg-3">                        
+                    <input type=submit  class="btn botao btn-primary"onclick="window.location.href='requisicoes_validar.php'" value="Deletar múltiplos">
+                </div>
+                <div class="col-6 col-sm-3 text-center col-md-3 col-lg-3">                        
+                    <button type="submit" class="btn botao btn-primary">Exportar</button>
+                </div>
+                <div class="col-6 col-sm-3 text-center col-md-3 col-lg-3">                        
+                    <input type="button" value="Adicionar" class="btn botao btn-primary" onclick="window.location.href='requisicoes_validar.php'" >
+                </div>
+            </div>
+            <section id="table-control justify-content-center">
+                <div class="table-control table-responsive">
+                    <table class="table">
                         <thead>
                             <tr>
                                 <th scope="col" class="table-id">ID da Compra - ID do Usuário</th>
@@ -143,15 +129,15 @@ require_once "listar_requisicoes.php";
                                     <td>
                                         <input type="checkbox" value ="<?php echo($list_1['id']); echo($list_1['iduser']);?>" name='box[]'>
                                     </td>
+                                <?php endwhile; ?>
                             </tr>
-                            <?php endwhile; ?>
                         </tbody>    
                     </table>
-            </div>
-        </section>
-                </form>
-            <?php
-                require_once "../Constructs/footer.php";
-            ?>
+                </div>
+            </section>
+        </form>
+        <?php
+            require_once "../Constructs/footer.php";
+        ?>
     </body>
 </html>
