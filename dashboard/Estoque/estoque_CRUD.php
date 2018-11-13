2127a36d8e10753fd48a393b61aab0761d333610
PauloRMacedo <44329969+PauloRMacedo@users.noreply.github.com>
2018-11-12 23:55:05 -0300

    Add files via upload

--- dashboard/Estoque/estoque_CRUD.php
+++ dashboard/Estoque/estoque_CRUD.php
@@ -5,115 +5,117 @@ require_once "listar_estoque.php";
 unset($_SESSION['user-edit']);
 ?>
 <html>
-<link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
-
-<!-- NAVBAR -->
-<div id="mySidenav" class="menu">
-<div id="icom" class = "icom">
-<img src = "../Images/user-icon.png" class="icom-img">
-</div>
-  <div id="list" class="list">
-      <div id="item" class="box">
-        
-        <!-- Menu principal -->
-            <a class="item navitens item_font" href="../Principal/principal.php">Principal</a>
-
-        <!-- Menu usuarios -->          
-            <a class="item navitens item_font" href="../Usuarios/usuarios.php">Usuários</a>
-
-        <!-- Menu requisições -->
-            <a class="item navitens item_font" href="../Requisicoes/requisicoes_CRUD.php">Requisições</a>
-
-        <!-- Menu estoque -->          
-            <a class="item navitens item_font click" href="../Estoque/estoque_CRUD.php">Estoque</a>
-
-        <!-- Menu sair -->
-            <a class="item navitens item_font" href="../Sair/sair.php">Sair</a>
-          
-      </div>
-  </div>
-</div>
-
-<title>Lista de Estoque</title>
-<link rel="stylesheet" type="text/css" href="../CSS/estoque_CRUD.css">
-<body>
-
-
-<div id="title" class="title">
-<h1>Lista de Estoque</h1>
-</div> <!--fim title-->
-<section>
-    <div class="form-group">
-<form action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-<div class="row">
-        <div class="col-3">
-            <label class="label-select">Buscar por</label>
-                <select name="tipo" class="form-control imput-select">
-                    <option value ="1">Categoria</option>
-                    <option value = "2">Nome</option>
-                    <option value = "3">ID</option>
-                </select>
+    <link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/estoque_CRUD.css">
+    <title>Lista de Estoque</title>
+    <!-- NAVBAR -->
+    <div id="mySidenav" class="menu">
+        <div id="icom" class = "icom">
+            <img src = "../Images/user-icon.png" class="icom-img">
         </div>
-        <div class="col-3">
-            <label class="label-serach">Item:</label>
-                <input name="busca" type="text" class="form-control imput-serach" placeholder="Nome do item">
-        </div>
-        <div class="col-3">
-            <label class="label-order">Ordenar por</label>
-                <select name="order" class="form-control imput-order">
-                    <option value= "1">Mais antigos</option>
-                    <option value = "2">Mais novos</option>
-                    <option value = "3">Ordem alfabética</option>
-                    <option value = "4">Maior dívida</option>
-                    <option value = "5">Menor dívida</option>
-                </select>
-            </form>      
-        </div>
+        <div id="list" class="list">
+            <div id="item" class="box">
+                
+                <!-- Menu principal -->
+                    <a class="item navitens item_font" href="../Principal/principal.php">Principal</a>
 
-        </div> <!--Fim da linha-->
+                <!-- Menu usuarios -->          
+                    <a class="item navitens item_font" href="../Usuarios/usuarios.php">Usuários</a>
 
-    <form method= "GET" action = "delete_multi.php">  
-    <button type="submit" class="btn btn-primary ">Buscar</button>
-    <input type=submit  class="btn btn-primary"onclick="window.location.href='delete_multi.php'" value="Deletar multiplos">
-    <button type="submit" class="btn btn-primary">Exportar</button>
-    <input type="button" value="Adicionar" class="btn btn-primary" onclick="window.location.href='estoque_criar.php'" >
+                <!-- Menu requisições -->
+                    <a class="item navitens item_font" href="../Requisicoes/requisicoes_CRUD.php">Requisções</a>
+
+                <!-- Menu estoque -->          
+                    <a class="item navitens item_font click" href="../Estoque/estoque_CRUD.php">Estoque</a>
+
+                <!-- Menu sair -->
+                    <a class="item navitens item_font" href="../Sair/sair.php">Sair</a>
+                
             </div>
         </div>
-        </div>
-        
-</div>
-</section>
-<section>
-    <div class="table-control table-responsive">
-    <table class="table">
-        <thead>
-            <tr>
-            <th scope="col" class="table-id">ID</th>
-            <th scope="col" class="table-nome">Nome - Categoria</th>
-            <th scope="col" class="table-date">Data de criação</th>
-            <th scope="col" class="table-preco">Preço (R$)</th>
-            <th scope="col" class="table-edit">Editar</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <?php   while ($list_1 = $stmt_1->fetch(PDO::FETCH_ASSOC)):?>
-            <td class="table-id"><?php echo($list_1['id']);?></td>
-            <td class="table-nome"><?php echo ''.$list_1['nome'].' - '.$list_1['catg'];?></td>
-            <td class="table-date"><?php echo($list_1['dadd']);?></td>
-            <td class="table-preco"><?php echo($list_1['preco']);?></td>
-            <td>
-                <a class="table-edit-icon" href="estoque_editar.php?id=<?php echo $list_1['id'];?>">
-                    <img class="form-icon" alt="edit-icon" src="../Images/edit.png" >
-                Editar
-                </a>
-                <a class="table-edit-icon" href="delete.php?id=<?php echo $list_1['id'];?>">
-                    <img class="form-icon" alt="edit-icon" src="../Images/delete.png" >
-                Deletar
-                </a>
-                <input type="checkbox" value ="<?php echo($list_1['id']);?>" name='box[]'></td>
-            </tr>
-                <?php endwhile; ?>
+    </div>
+
+    <body>
+        <div id="title" class="title">
+            <h1>Lista de Estoque</h1>
+        </div> <!--fim title-->
+        <section>
+            <div class="form-group">
+                <form action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                    <div class="row">
+                        <!-- NA DIV DE BAIXO EU DEIXEI col-4 NO CASO DE TU AJEITAR A NAVBAR, SE NÃO CONSEGUIR MUDA PARA col-6 -->
+                        <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
+                            <label class="ml-2 label-select">Buscar por</label>
+                            <select name="tipo" class="form-control imput-select">
+                                <option value ="1">Categoria</option>
+                                <option value = "2">Nome</option>
+                                <option value = "3">ID</option>
+                            </select>
+                        </div>
+                        <!-- NA DIV DE BAIXO EU DEIXEI col-4 NO CASO DE TU AJEITAR A NAVBAR, SE NÃO CONSEGUIR MUDA PARA col-6 -->
+                        <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
+                            <label class="ml-2 label-serach">Item:</label>
+                            <input name="busca" type="text" class="form-control imput-serach" placeholder="Nome do item">
+                        </div>
+                        <!-- NA DIV DE BAIXO EU DEIXEI col-4 NO CASO DE TU AJEITAR A NAVBAR, SE NÃO CONSEGUIR MUDA PARA col-6 -->
+                        <div class="mt-3 col-4 col-sm-4 col-md-4 col-lg-4"> 
+                            <label class="mx-2 label-order">Ordenar por:</label>
+                            <select name="order" class="form-control imput-order">
+                                <option value= "1">Mais antigos</option>
+                                <option value = "2">Mais novos</option>
+                                <option value = "3">Ordem alfabética</option>
+                                <option value = "4">Maior dívida</option>
+                                <option value = "5">Menor dívida</option>
+                            </select>
+                        </div>
+                    </div> 
+                </form>      
+
+                <!--Fim da linha-->
+
+                <form method= "GET" action = "delete_multi.php">  
+                    <button type="submit" class="btn btn-primary ">Buscar</button>
+                    <input type=submit  class="btn btn-primary"onclick="window.location.href='delete_multi.php'" value="Deletar multiplos">
+                    <button type="submit" class="btn btn-primary">Exportar</button>
+                    <input type="button" value="Adicionar" class="btn btn-primary" onclick="window.location.href='estoque_criar.php'" >
+                </form>
             </div>
-</form>
-</section>
+        </section>
+
+        <section>
+            <div class="table-control table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col" class="table-id">ID</th>
+                        <th scope="col" class="table-nome">Nome - Categoria</th>
+                        <th scope="col" class="table-date">Data de criação</th>
+                        <th scope="col" class="table-preco">Preço (R$)</th>
+                        <th scope="col" class="table-edit">Editar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <?php   while ($list_1 = $stmt_1->fetch(PDO::FETCH_ASSOC)):?>
+                        <td class="table-id"><?php echo($list_1['id']);?></td>
+                        <td class="table-nome"><?php echo ''.$list_1['nome'].' - '.$list_1['catg'];?></td>
+                        <td class="table-date"><?php echo($list_1['dadd']);?></td>
+                        <td class="table-preco"><?php echo($list_1['preco']);?></td>
+                        <td>
+                            <a class="table-edit-icon" href="estoque_editar.php?id=<?php echo $list_1['id'];?>">
+                                <img class="form-icon" alt="edit-icon" src="../Images/edit.png" >
+                                Editar
+                            </a>
+                            <a class="table-edit-icon" href="delete.php?id=<?php echo $list_1['id'];?>">
+                                <img class="form-icon" alt="edit-icon" src="../Images/delete.png" >
+                                Deletar
+                            </a>
+                            <input type="checkbox" value ="<?php echo($list_1['id']);?>" name='box[]'>
+                        </td>
+                    </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </section>
+    </body>
+</html>
--- dashboard/Estoque/estoque_criar.php
+++ dashboard/Estoque/estoque_criar.php
@@ -5,95 +5,101 @@ require_once "../Constructs/header.php";
 
 ?>
 <html>
-<link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/estoque_criar.css">
+    <title>Adicionar produto</title>
+    
+    <!-- NAVBAR -->
+    <div id="mySidenav" class="menu">
+        <div id="icom" class = "icom">
+            <img src = "../Images/user-icon.png" class="icom-img">
+        </div>
+        <div id="list" class="list">
+            <div id="item" class="box">
+                
+            <!-- Menu principal -->
+                <a class="item navitens item_font" href="../Principal/principal.php">Principal</a>
 
-<!-- NAVBAR -->
-<div id="mySidenav" class="menu">
-<div id="icom" class = "icom">
-<img src = "../Images/user-icon.png" class="icom-img">
-</div>
-  <div id="list" class="list">
-      <div id="item" class="box">
-        
-        <!-- Menu principal -->
-            <a class="item navitens item_font" href="../Principal/principal.php">Principal</a>
+            <!-- Menu usuarios -->          
+                <a class="item navitens item_font" href="../Usuarios/usuarios.php">Usuários</a>
 
-        <!-- Menu usuarios -->          
-            <a class="item navitens item_font" href="../Usuarios/usuarios.php">Usuários</a>
+            <!-- Menu requisições -->
+                <a class="item navitens item_font" href="../Requisicoes/requisicoes_CRUD.php">Requisições</a>
 
-        <!-- Menu requisições -->
-            <a class="item navitens item_font" href="../Requisicoes/requisicoes_CRUD.php">Requisições</a>
+            <!-- Menu estoque -->          
+                <a class="item navitens item_font click" href="../Estoque/estoque_CRUD.php">Estoque</a>
 
-        <!-- Menu estoque -->          
-            <a class="item navitens item_font click" href="../Estoque/estoque_CRUD.php">Estoque</a>
-
-        <!-- Menu sair -->
-            <a class="item navitens item_font" href="../Sair/sair.php">Sair</a>
-          
-      </div>
-  </div>
-</div>
-
-<title>Adicionar produto</title>
-<link rel="stylesheet" type="text/css" href="../CSS/estoque_criar.css">
-<body>
-
-
-<div id="title" class="title">
-<h1>Adicionar produto</h1>
-</div> <!--fim title-->
-
-<section>
-    <div class="form-group">
-
-        <form action="validar_cadastro.php" method="POST" enctype="multipart/form-data">
-        <?php if (!(empty($_SESSION['dbug']))) {
-            echo'<h3>'.$_SESSION['dbug'].'</h3>';
-            unset($_SESSION['dbug']);
-            }
-        ?>
-            <label class="label-nome">Nome do item</label>
-            <?php if (!(empty($_SESSION['nome-e']))) {
-                echo'<a>'.$_SESSION['nome-e'].'</a>';
-                unset($_SESSION['nome-e']);
-            }?>
+            <!-- Menu sair -->
+                <a class="item navitens item_font" href="../Sair/sair.php">Sair</a>
             
-            <input required type="text" name="nome" class="form-control" placeholder="Nome do item">
-            <label class="label-nome">Categoria</label>
-                <select class="form-control" name="cat">
-                    <option value="hortfruit">Horti-Fruti</option>
-                    <option value="congelados">Congelados</option>
-                </select>
-
-            <label class="label-nome">Email do administrador</label>
-            <?php if (!(empty($_SESSION['adm-mail-e']))) {
-            echo'<a>'.$_SESSION['adm-mail-e'].'</a>';
-            unset($_SESSION['adm-mail-e']);
-            }?>
-            <input required type="text" name="adm-mail" class="form-control" placeholder="E-mail">
-        
-            <label class="label-nome">Senha do administrador</label>
-            <?php if (!(empty($_SESSION['adm-pass-e']))) {
-                echo'<a>'.$_SESSION['adm-pass-e'].'</a>';
-                unset($_SESSION['adm-pass-e']);
-            }?>
-            <input required type="password" name="adm-pass" class="form-control" placeholder="Senha">
-            
-            
-            <label class="label-file">Selecionar Imagen</label>
-                    <input type="file" name="file-img" class=" form-control form-img">
-            
-                    <label class="label-preco">Preço</label>
-            <?php if (!(empty($_SESSION['preco-e']))) {
-            echo '<a>'.$_SESSION['preco-e'].'</a>';
-            unset($_SESSION['preco-e']);
-            }
-            ?>
-            <input type="text" name="preco" class="form-control imput-debit" placeholder="Ex 00,00 (R$)">
-            <button type="submit" class="btn btn-primary bt-valid">Validar</button>
-            <input type="button" value="Sair" class="btn btn-primary" onclick="window.location.href='estoque_CRUD.php'" >
-            <button type="reset" class="btn btn-primary bt-valid">Limpar</button>
-        </form>
+            </div>
+        </div>
     </div>
-</section>
+    
+    <body>
+        <div id="title" class="row title">
+            <h1 class="justify-content-center col-12">Adicionar produto</h1>
+        </div> <!--fim title-->
+
+        <section>
+            <div class="form-group">
+                <form action="validar_cadastro.php" method="POST" enctype="multipart/form-data">
+                    <?php if (!(empty($_SESSION['dbug']))) {
+                        echo'<h3>'.$_SESSION['dbug'].'</h3>';
+                        unset($_SESSION['dbug']);
+                        }
+                    ?>
+                    <label class="label-nome ml-3 mt-4">Nome do item</label>
+                    <?php if (!(empty($_SESSION['nome-e']))) {
+                        echo'<a>'.$_SESSION['nome-e'].'</a>';
+                        unset($_SESSION['nome-e']);
+                    }?>
+                    
+                    <input required type="text" name="nome" class="form-control" placeholder="Nome do item">
+                    <label class="label-nome ml-3 mt-4">Categoria</label>
+                    <select class="form-control" name="cat">
+                        <option value="hortfruit">Horti-Fruti</option>
+                        <option value="congelados">Congelados</option>
+                    </select>
+
+                    <label class="label-nome ml-3 mt-4">Email do administrador</label>
+                    <?php if (!(empty($_SESSION['adm-mail-e']))) {
+                    echo'<a>'.$_SESSION['adm-mail-e'].'</a>';
+                    unset($_SESSION['adm-mail-e']);
+                    }?>
+                    <input required type="text" name="adm-mail" class="form-control" placeholder="E-mail">
+        
+                    <label class="label-nome ml-3 mt-4">Senha do administrador</label>
+                    <?php if (!(empty($_SESSION['adm-pass-e']))) {
+                        echo'<a>'.$_SESSION['adm-pass-e'].'</a>';
+                        unset($_SESSION['adm-pass-e']);
+                    }?>
+                    <input required type="password" name="adm-pass" class="form-control" placeholder="Senha">
+                    
+                    
+                    <label class="label-file ml-3 mt-4">Selecionar Imagem</label>
+                    <input type="file" name="file-img" class=" form-control form-img">
+                    <label class="label-preco ml-3 mt-4">Preço</label>
+                    <?php if (!(empty($_SESSION['preco-e']))) {
+                        echo '<a>'.$_SESSION['preco-e'].'</a>';
+                        unset($_SESSION['preco-e']);
+                    }
+                    ?>
+                    <input type="text" name="preco" class="form-control imput-debit" placeholder="Ex 00,00 (R$)">
+                    <div class="row container">
+                        <div class="col-4 col-sm-4 col-md-4 col-lg-4 mb-4">
+                            <button type="submit" class="btn btn-primary bt-valid">Validar</button>
+                        </div>    
+                        <div class="col-4 col-sm-4 col-md-4 col-lg-4 mb-4">
+                            <input type="button" value="Sair" class="btn btn-primary" onclick="window.location.href='estoque_CRUD.php'" >
+                        </div>
+                        <div class="col-4 col-sm-4 col-md-4 col-lg-4 mb-4">
+                            <button type="reset" class="btn btn-primary bt-valid">Limpar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </body>
+</html>
 
