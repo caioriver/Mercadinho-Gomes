377fd5f56cedbca4cd43885aac141e183a96e96e
caioriver <43602376+caioriver@users.noreply.github.com>
2018-11-12 22:45:16 -0300

    Add files via upload

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,25 +1,19 @@
 <div class="redes-cor">
     <div class="row justify-content-center ">
-        <div class="mg-redes text-center col-3 col-sm-1 col-md-2 col-lg-1">
-            <a href=""><img src="icones/instagram-branco.png" width="25"></a>
-        </div>
-        <div class="mg-redes text-center col-2 col-sm-1 col-md-2 col-lg-1">
-            <a href=""><img src="icones/facebook-branco.png" width="25"></a>
-        </div>
-        <div class="mg-redes text-center col-7 col-sm-4 col-md-4 col-lg-2">
+        <div class="mg-redes text-center col-sm-5 col-md-5 col-lg-3">
             <img src="icones/telephone-handle-silhouette.png" width="25">
-            <span class="ml-1">(71)9XXXX-XXXX</span><br>
+            <span class="ml-1">(71)3316-2505</span><br>
         </div>
-        <div class="mg-redes text-center col-sm-4 col-md-4 col-lg-3">
+        <div class="mg-redes text-center col-sm-5 col-md-6 col-lg-4">
             <img src="icones/message-closed-envelope.png" width="25">
-            <span class="ml-1">email@contato.com</span><br>
+            <span class="ml-1">contatomercadinho@gomes.com.br</span><br>
         </div>
         <div class="mg-redes text-center col-md-12 col-lg-5">
             <img src="icones/wall-clock.png" width="25">
-            <span class="ml-1">Segunda a Sexta: 8h às 18h Sábados: 8h às 15h</span>
+            <span class="ml-1">Segunda a Sexta: 9h às 19h Sábados: 8h às 12h</span>
         </div>
     </div>
 </div>    
 <div  class="ftr-cor">
-    Desenvolvido por a Praxis - Empresa Jr. © 2018 Todos os direitos reservados.
+    Desenvolvido por a <a href="https://praxisjr.com.br/" target="_blank"> Praxis - Empresa Jr. © </a> 2018 Todos os direitos reservados.
 </div>
