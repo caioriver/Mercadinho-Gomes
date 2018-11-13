23a0ec608988429b6ebfbcce6d51f8ca3757aa97
caioriver <43602376+caioriver@users.noreply.github.com>
2018-11-13 12:10:40 -0300

    Add files via upload

--- dashboard/admin/admin_editar.php
+++ dashboard/admin/admin_editar.php
@@ -1,11 +1,6 @@
-
-
 <!-- header -->
 <?php
 require_once "../Constructs/header.php";
-if ($_SESSION['type-user'] != "super") {
-    header("Location: ../Principal/principal.php");
-} else {
 require_once "../../fun/_fixed.php";
 $connect = db_connect();
 // pega o ID da URL
@@ -19,14 +14,12 @@ $_SESSION['id-up'] = $id;
     $stmt_1->bindValue(':id',$_SESSION['id-up']);
     $stmt_1->execute();
     $user_1 = $stmt_1->fetch(PDO::FETCH_ASSOC);
-}
+
 ?>
 
-<link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
-<link rel="stylesheet" type="text/css" href="../CSS/usuarios_criar.css">
-
-
 <html>
+
+<link rel="stylesheet" type="text/css" href="../CSS/navbar_dashboard.css">
 
 <body>
     
@@ -40,12 +33,12 @@ $_SESSION['id-up'] = $id;
             </div>            
             <ul class="list-unstyled components menuu">
                 <div class="cordefundo">
-                    <li>
+                    <li class="active">
                         <a href="../Principal/principal.php">
                             Principal
                         </a>
                     </li>
-                    <li class="active">
+                    <li>
                         <a href="../Usuarios/usuarios.php">
                             Usuarios
                         </a>
@@ -84,118 +77,125 @@ $_SESSION['id-up'] = $id;
                     </button>
                 
                     <div class="title justify-content-center text-center">
-                        <h1>Editar Administrador</h1>
+                        <h1>Principal</h1>
+                        <?php 
+                            if (!(empty($_SESSION['dbug']))) {
+                                echo '<h3>'.$_SESSION['dbug'].'</h3>';
+                                unset($_SESSION['dbug']);
+                                    }
+                                    ?>
+                                    <?php echo $id;?>
                     </div>
 
                 </div>
 
             </nav>
 
-
             <section>
                 <div class="form-group">
-                    <?php if (!(empty($_SESSION['dbug']))) {
-                        echo '<h3>'.$_SESSION['dbug'].'</h3>';
-                        unset($_SESSION['dbug']);
-                            }
-                    ?>
-                    <form action="validar_criar.php" method="POST">
-                        <label class="ml-4 mt-3 label-nome">Inserir nome</label>
+
+                    <form action="validar_editar.php" method="POST">
+                        <label class="label-nome">Inserir nome</label>
                         <?php if (!(empty($_SESSION['nome-e']))) {
                         echo '<a>'.$_SESSION['nome-e'].'</a>';
                         unset($_SESSION['nome-e']);
                             }
                         ?>
-                        <input type="text" name="nome" class="form-control" name="nome" placeholder="Nome completo">        
+                        <input value ="<?php echo $user_1['nome'];?>"type="text" name="nome" class="form-control" name="nome" placeholder="Nome completo">
+                    
                         <div class="row">
                             <div class="col-6">
-                                <label class="ml-4 mt-3 label-mail">E-mail</label>                        
+                                <label class="label-mail">E-mail</label>
                                 <?php if (!(empty($_SESSION['mail-e']))) {
                                     echo '<a>'.$_SESSION['mail-e'].'</a>';
                                     unset($_SESSION['mail-e']);
-                                }?>                            
-                                <input type="email" name="mail" class="form-control imput-email" placeholder="E-mail">
+                                } ?>
+                                <input value ="<?php echo $user_1['mail'];?>"type="email" name="mail" class="form-control imput-email" placeholder="E-mail">
                             </div>
+            
                             <div class="col-6">
-                                <label class="ml-4 mt-3 label-cpf">CPF</label>
+                                <label class="label-cpf">CPF</label>
                                 <?php if (!(empty($_SESSION['cpf-e']))) {
                                     echo '<br><a>'.$_SESSION['cpf-e'].'</a>';
                                     unset($_SESSION['cpf-e']);
-                                }?>
-                                <input type="text" name="cpf" class="form-control imput-cpf" placeholder="Ex: 000.000.00-00">
+                                } ?>
+                                <input value ="<?php echo $user_1['cpf'];?>" type="text" name="cpf" class="form-control imput-cpf" placeholder="Ex: 000.000.00-00">
                             </div>
                         </div> <!--fim linha duas colunas-->
+
                         <div class="row">
                             <div class="col-6">
-                                <label class="ml-4 mt-3 label-confirm-mail">Confirmar E-mail</label>
+                                <label class="label-confirm-mail">Confirmar E-mail</label>
                                 <?php if (!(empty($_SESSION['c-mail-e']))) {
                                     echo '<a>'.$_SESSION['c-mail-e'].'</a>';
                                     unset($_SESSION['c-mail-e']);
-                                }?>
+                                } ?>
                                 <input type="email" name="c-mail" class="form-control imput-email" placeholder="Confirmar E-mail">
-                            </div>        
+                            </div>
+                        
                             <div class="col-6">
-                                <label class="ml-4 mt-3 label-tel">Telefone</label>
+                                <label class="label-tel">Telefone</label>
                                 <?php if (!(empty($_SESSION['tel-e']))) {
                                     echo '<a>'.$_SESSION['tel-e'].'</a>';
                                     unset($_SESSION['tel-e']);
-                                }?>
-                                <input type="text" name="tel" class="form-control imput-cpf" placeholder="Ex: (DD) 0000-0000">
-                            </div   >
+                                } ?>
+                                <input value ="<?php echo $user_1['tel'];?>" type="text" name="tel" class="form-control imput-cpf" placeholder="Ex: (DD) 0000-0000">
+                            </div>
                         </div> <!--fim linha duas colunas-->
 
-                        <label class="ml-4 mt-3 label-endereco">Endereço</label>
+                        <label class="label-endereco">Endereço</label>
                         <?php if (!(empty($_SESSION['end-e']))) {
-                            echo '<a>'.$_SESSION['end-e'].'</a>';
-                            unset($_SESSION['end-e']);
-                        }?>        
-                        <input type="text" name="end" class="form-control" placeholder="Endereço">
+                                echo '<a>'.$_SESSION['end-e'].'</a>';
+                                unset($_SESSION['end-e']);
+                            } ?>        
+                        <input value ="<?php echo $user_1['endc'];?>" type="text" name="end" class="form-control" placeholder="Endereço">
                         
                         <div class="row">
-                            <div class="col-6">
-                                <label class="ml-4 mt-3 label-senha">Senha</label>
+                            <div class="col-4">
+                                <label class="label-senha">Senha</label>
                                 <?php if (!(empty($_SESSION['pass-e']))) {
                                     echo '<a>'.$_SESSION['pass-e'].'</a>';
                                     unset($_SESSION['pass-e']);
-                                }?>  
+                                } ?>  
                                 <input type="password" name="pass" class="form-control imput-senha" placeholder="Senha">
                             </div>
-                            <div class="col-6">
-                                <label class="ml-4 mt-3 label-confirm-senha">Confirmar senha</label>
+                        
+                            <div class="col-4">
+                                <label class="label-confirm-senha">Confirmar senha</label>
                                 <?php if (!(empty($_SESSION['c-pass-e']))) {
                                     echo '<a>'.$_SESSION['c-pass-e'].'</a>';
                                     unset($_SESSION['c-pass-e']);
-                                }?>    
+                                } ?>    
                                 <input type="password" name="c-pass" class="form-control imput-confirm-senha" placeholder="Confirmar senha">
                             </div>
                         </div> <!--fim linha duas colunas-->
 
-                        <label class="ml-4 mt-3 label-senha-adn">Senha do administrador principal</label>
+                        <label class="label-senha-adn">Senha do administrador principal</label>
                         <?php if (!(empty($_SESSION['adm-pass-e']))) {
                                 echo '<a>'.$_SESSION['adm-pass-e'].'</a>';
                                 unset($_SESSION['adm-pass-e']);
-                            }?>      
-                        <input type="password" name="adm-pass" class="form-control" placeholder="senha">    
-                        <div class="row">
-                            <div class="col-6 col-sm-4 col-md-4 col-lg-4">
-                                <button type="submit" class="btn btn-primary botao bt-valid">Validar</button>
-                            </div>
-                            <div class="col-6 col-sm-4 col-md-4 col-lg-4">
-                                <button type="reset" class="btn btn-primary botao bt-valid">Limpar</button>    
-                            </div>
-                            <div class="col-12 col-sm-4 col-md-4 col-lg-4">
-                                <input type="button" value="Sair" class="btn btn-primary botao mb-4" onclick="window.location.href='usuarios.php'" >
-                            </div>
-                        </div>
+                            } ?>      
+                        <input type="password" name="adm-pass" class="form-control" placeholder="senha">
+                                                            
+                        <label class="label-debit">Débito Inicial</label>
+                        <input type="number" name="debit" class="form-control imput-debit" placeholder="Ex 00,00 (R$)">
+                
+                    
+                        <button type="submit" class="btn botao btn-primary bt-valid">Validar</button>
+                        
+                        <input type="button" value="Sair" class="btn botao btn-primary" onclick="window.location.href='admin_CRUD.php'" >
+                        
+                        <button type="reset" class="btn botao btn-primary bt-valid">Limpar</button>
+                    
                     </form>
                 </div>
             </section>
         </div>
     </div>
-    
+
     <?php
         require_once "../Constructs/footer.php"
     ?>
-    
+            
 </body>
 </html>
