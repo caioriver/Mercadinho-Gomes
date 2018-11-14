9dfd359ec7287050f4602def5b1a8fdc9e62ded5
caioriver <43602376+caioriver@users.noreply.github.com>
2018-11-13 23:37:58 -0300

    Update delete_multi.php

--- dashboard/Usuarios/delete_multi.php
+++ dashboard/Usuarios/delete_multi.php
@@ -3,7 +3,7 @@ session_start();
 // COLETAR DADOS DAS TURMAS EXISTENTES
 require_once "../../fun/_fixed.php";
 // CONEXÃO
-if (empty($_GET['box'])) {
+if (iiset($_GET['box'])) {
     $_GET['box'] = null;
     header("Location: usuarios_CRUD.php");
 }
@@ -12,11 +12,9 @@ $cx = db_connect();
 $_checkbox = $_GET['box'];
 foreach($_checkbox as $_valor){
 $query1 = 'DELETE FROM usuarios WHERE id = ' .$_valor.';';
-
 $insert1 = $cx->prepare($query1);
 $insert1->execute();
 }
-
 header("Location: usuarios_CRUD.php");
 } else {
     header("Location: usuarios_CRUD.php");
